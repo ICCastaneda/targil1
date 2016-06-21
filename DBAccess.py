af24282d8d39a9b5c82cd0a191b2ac8c25073264
imsrazm1 <imsrazm@gmail.com>
2016-06-21 05:58:40 +0300

    test to seprate dir, use mysql.connector, clean code

--- DBAccess.py
+++ DBAccess.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python2.7
-import MySQLdb
+# import mysql.connector as mariadb     # mysqldb   # MySQLdb
+# import mysqldb
+import mysql.connector
 import datetime
 import CONS
 
 
 def bld_sql(start_date, end_date, event_title):
-    where_str = ""
+    where_str = None
     if start_date and end_date:
         where_str = "event_date >= start_date and event_date <= end_date"
     elif start_date and not end_date:
@@ -16,41 +17,57 @@ def bld_sql(start_date, end_date, event_title):
         where_str = "event_date > ' '"
 
     if event_title:
-        where_str += " and event_title = '{}'".format(event_date) 
+        where_str += " and event_title = '{}'".format(event_title)
  
     sqlx_count = "select count(*) from events where ({})".format(where_str)
-    sqlx = "select * from events where ({}).format(where_str)
+    sqlx = "select * from events where ({})".format(where_str)
     return [sqlx, sqlx_count]
+
 
 def bld_add_sql(title1, date1, desc1):
     insert_str = "insert tg1_events (event_title, event_date, event_desc) \
         values( '{}' '{}' '{}'".format(title1, date1, desc1)
 
-    return inser_sql 
+    return insert_str
+
+
+def add_event(sqlx):
+
+    ap = db_create_connection()
+    rmsg = ""
+    try:
+        db_exec_cur(ap.cur, sqlx, "desc")
+
+    except Exception as e:
+        rmsg =  ["error", "error adding event"]
+
+    db_close_con(ap)
+    if not rmsg:
+        rmsg = ["success", "event added succesfully"]
+    return rmsg
 
 
 def get_events(sqlx_count, sqlx):
 
     ap = db_create_connection()
-    ap.cur = db_create_cur(ap)
-    
+
     try:
         obj_count = {"count": -1}
-        rslt = ap_exec_cur(ap.cur, sqlx_count, "no_desc")
+        rslt = db_exec_cur(ap.cur, sqlx_count, "no_desc")
         for t in rslt[0]:
             obj_count["count"] = t[0]
         
-        ap_exec_cur(ap.cur, sqlx, "desc")    
-    execpt Exception as e:
+        db_exec_cur(ap.cur, sqlx, "desc")
+    except Exception as e:
         raise "error reading the database"
 
     rslt_list = convert_tuple(ap.result, obj_count)
-    ap_close_con(ap)
+    db_close_con(ap)
     return rslt_list
 
 
 def convert_tuple(ap, obj_count):
-    a_result[0] = obj_count
+    a_result = [obj_count]
     for t in ap.result:
         cnt1 = -1
         uj = {}
@@ -63,24 +80,30 @@ def convert_tuple(ap, obj_count):
     return ['ok', a_result]
     
 
-
 def db_create_connection():
-    ap = dbaccess()    
-    ap.con = MySQLdb.connect(CONS.HOST_NAME, CONS.USER, CONS.PW, CONS.DB_NAME)
-    ap.cur = ap.con.cursor
+    ap = DBAccess()
+    cnx = mysql.connector.connect(user='scott', password='tiger',
+                                  host='127.0.0.1',
+                                  database='employees')
+
+    ap.con = mysql.connector.connect(user=CONS.USER, password=CONS.PW,
+                                     database=CONS.DB_NAME, host=CONS.HOST)
+    ap.cur = ap.con.cursor()
     return ap
+
 
 def db_close_con(ap):
     ap.con.close()
 
-def db_fetchall(ap):
+
+def db_exec_cur(ap):
     ap.cursor.execute(ap.sqlx)
     ap.result = ap.cur.fetchall()
     ap.desc = [x[0].lower for x in ap.cur.description] 
     return ap
 
 
-class dbaccess(object):
+class DBAccess(object):
     con = None
     cur = None
     result = None
